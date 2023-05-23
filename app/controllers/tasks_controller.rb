f45788e0d5a1b2a0eaba65cab774904d651de7b0
Carolina Rusche <carolinarusche@gmail.com>
2023-05-23 15:50:02 +0200

    Challenge almost finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,29 @@ class TasksController < ApplicationController
   end
 
   def new
-    @taks = Task.new
+    @task = Task.new
   end
 
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     @task.save
     redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,19 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#details', as: :task
-  get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+
+ # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
